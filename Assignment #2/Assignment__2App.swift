0c2ce5bebe960389a3781a4a47e39eb3f53cd16d
ruaasaad <100333163+ruaasaad@users.noreply.github.com>
2023-02-14 21:35:59 -0800

    Initial Commit

---
+++ Assignment #2/Assignment__2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment__2App.swift
+//  Assignment #2
+//
+//  Created by Ruaa Alsaba on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment__2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
